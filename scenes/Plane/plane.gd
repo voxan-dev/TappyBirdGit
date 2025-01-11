4860ca9c5626cf3afd532aece9f35bd4c598b0e2
voxan-dev <sdorr87@gmail.com>
2025-01-11 14:17:24 +0100

    Initial Commit with staged changes: video 54 done

---
+++ scenes/Plane/plane.gd
@@ -0,0 +1,32 @@
+extends CharacterBody2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var engine_sound: AudioStreamPlayer2D = $EngineSound
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+const GRAVITY: float = 1000.0
+const POWER: float = -350.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	velocity.y += GRAVITY * delta
+	fly()
+	move_and_slide()
+	if is_on_floor():
+		die()
+	
+		
+		
+func fly() -> void:
+	if Input.is_action_pressed("jump"):
+		velocity.y = POWER
+		animation_player.play("power")
+
+func die() -> void:
+	set_physics_process(false)
+	engine_sound.stop()
+	animated_sprite_2d.stop()
