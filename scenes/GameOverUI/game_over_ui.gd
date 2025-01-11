9cada847a72aff76e34e3b1ff7c09acdab00f7a3
voxan-dev <sdorr87@gmail.com>
2025-01-11 21:12:16 +0100

    Video 70 done

---
+++ scenes/GameOverUI/game_over_ui.gd
@@ -0,0 +1,30 @@
+extends Control
+@onready var game_over_sound: AudioStreamPlayer = $GameOverSound
+@onready var game_over_label: Label = $GameOverLabel
+@onready var space_label: Label = $SpaceLabel
+@onready var timer: Timer = $Timer
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	hide()
+	SignalManager.on_plane_died.connect(on_plane_died)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if (
+		space_label.visible and 
+		Input.is_action_just_pressed("jump")
+		):
+			GameManager.load_main_scene()
+
+
+func _on_timer_timeout() -> void:
+	game_over_label.hide()
+	space_label.show()
+
+func on_plane_died() -> void:
+	show()
+	timer.start()
+	game_over_sound.play()
--- singletons/GameManager.gd
+++ singletons/GameManager.gd
@@ -1,6 +1,7 @@
 extends Node
 
 const GAME: PackedScene = preload("res://scenes/Game/game.tscn")
+const MAIN = preload("res://scenes/MainUI/main.tscn")
 
 const SCROLL_SPEED: float = -120.0
 const OFF_SCREEN : float = -500
@@ -8,3 +9,6 @@ const GROUP_PLANE: String = "Plane"
 
 func load_game_scene() -> void:
 	get_tree().change_scene_to_packed(GAME)
+
+func load_main_scene() -> void:
+	get_tree().change_scene_to_packed(MAIN)
