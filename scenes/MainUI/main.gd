20f6060af121dd3b2139fdd4a4903beee734ca98
voxan-dev <sdorr87@gmail.com>
2025-01-11 20:54:48 +0100

    Video 68 done

--- scenes/MainUI/main.gd
+++ scenes/MainUI/main.gd
@@ -10,4 +10,5 @@ func _ready() -> void:
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("jump"):
+		GameManager.load_game_scene()
--- singletons/GameManager.gd
+++ singletons/GameManager.gd
@@ -1,5 +1,10 @@
 extends Node
 
+const GAME: PackedScene = preload("res://scenes/Game/game.tscn")
+
 const SCROLL_SPEED: float = -120.0
 const OFF_SCREEN : float = -500
 const GROUP_PLANE: String = "Plane"
+
+func load_game_scene() -> void:
+	get_tree().change_scene_to_packed(GAME)
